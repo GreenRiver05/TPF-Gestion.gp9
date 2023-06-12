5d4fb5463299a24da8318afc682395120db2de86
GreenRiver05 <gabrielbd94@gmail.com>
2023-06-12 01:16:14 -0300

    ProyectoData
    Co-Authored-By: Pablo1864 <130506261+Pablo1864@users.noreply.github.com>
    Co-Authored-By: Maryolivera <128436041+Maryolivera@users.noreply.github.com>
    Co-Authored-By: alex-astudillo <105954347+ast-alex@users.noreply.github.com>

--- TPF_Gestion/src/AccesoADatos/ProyectoData.java
+++ TPF_Gestion/src/AccesoADatos/ProyectoData.java
@@ -90,7 +90,88 @@ public class ProyectoData {
         return listaProyecto;
 
     }
+    
+    public void buscarPorNombre(String nombre){
+        String sql = "SELECT * FROM proyecto WHERE Nombre=?";
+        Proyecto proyecto = new Proyecto();
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, nombre);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                
+                proyecto.setIdProyecto(rs.getInt("idProyecto"));
+                proyecto.setNombre(rs.getString("nombre"));
+                proyecto.setDescripcion(rs.getString("descripcion"));
+                proyecto.setFechaInicial(rs.getDate("FechaIncial").toLocalDate());
+                proyecto.setEstado(rs.getBoolean("Estado"));
 
+            } else {
+                JOptionPane.showMessageDialog(null, "Proyecto Inexistente");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Proyecto " + ex.getMessage());
+        }
+
+        return proyecto;
+        
+    }
+    
+    public void modificarProyecto(Proyecto proyecto){
+       String sql = "UPDATE proyecto SET Nombre=?, Descripcion=?, FechaInicial=?, Estado=? WHERE Nombre=?";
+       try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, proyecto.getNombre());
+            ps.setString(2, proyecto.getDescripcion());
+            ps.setDate(3, Date.valueOf(proyecto.getFechaInicial()));
+            ps.setBoolean(4, proyecto.isEstado());
+            ps.setString(5, proyecto.getNombre());
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Proyecto modificado exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Proyecto no existe");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Proyecto" + ex.getMessage());
+        }
+    }
+    
+    public void finalizado(int id){
+       String sql = "UPDATE proyecto SET  Estado=1 WHERE IdProyecto=?";
+       try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+           
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Proyecto Finalizado.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Proyecto no existe");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Proyecto" + ex.getMessage());
+        }
+    }
+
+    public void enProceso(int id){
+          String sql = "UPDATE proyecto SET  Estado=0 WHERE IdProyecto=?";
+       try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+           
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Proyecto en Proceso.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Proyecto no existe");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Proyecto" + ex.getMessage());
+        }
+    }
     
     
     
--- TPF_Gestion/src/tpf_gestion/TPF_Gestion.java
+++ TPF_Gestion/src/tpf_gestion/TPF_Gestion.java
@@ -1,15 +1,13 @@
-
 package tpf_gestion;
 
-import AccesoADatos.EquipoData;
-import Entidades.Equipo;
-
+import AccesoADatos.*;
+import Entidades.*;
+import java.time.LocalDate;
 
 public class TPF_Gestion {
 
-  
     public static void main(String[] args) {
-        ..............CREAR EQUIPO.................
+//     ..............CREAR EQUIPO.................
 //         Proyecto pro =new Proyecto("SSS","Prueba  datos",LocalDate.of(2022,3,1),true);
 //         ProyectoData pd=new ProyectoData();
 //         pd.crearProyectos(pro);
@@ -19,40 +17,32 @@ public class TPF_Gestion {
 //         EquipoData ed=new EquipoData();
 //         ed.crearEquipo(eq);
 //       
-          
+
 //...............BUSCAR POR NOMBRE............
-//      EquipoData id=new EquipoData();
-//      System.out.println("Equipo :"+id.buscarEquipo("Equipo 2").toString());
-        
-//...............LISTAR EQUIPOS.................................
-       EquipoData e=new EquipoData();
-       System.out.println("EQUIPOS");
-       for(Equipo equip : e.listarEquipos(true)){
-           System.out.println(equip.toString());
-       
+//        EquipoData id = new EquipoData();
+//        System.out.println("Equipo :" + id.buscarEquipo("Equipo 2").toString());
+//
+////...............LISTAR EQUIPOS.................................
+//        System.out.println("\n\n---------------------------------------------------------------------------------------------------\n\n");
+//
+//        EquipoData e = new EquipoData();
+//        System.out.println("EQUIPOS");
+//        for (Equipo equip : e.listarEquipos(true)) {
+//            System.out.println(equip.toString());
 //***********BAJA Y ALTA  EQUIPOS**************
-        EquipoData equi=new EquipoData();
-        equi.bajaEquipo(6);
-        equi.altaEquipo(5);
-    
-        
-   
+//        EquipoData equi = new EquipoData();
+//        equi.bajaEquipo(5);
+//        equi.altaEquipo(6);
+
 //*********MODIFICAR**************
-
-//         Proyecto pro =new Proyecto("Prueba","Prueba  datos",LocalDate.of(2022,3,1),true);
-//         ProyectoData pd=new ProyectoData();
-//         pd.crearProyectos(pro);
-//          
-//          
-//         Equipo eq=new Equipo (pro,"Equipo 32",LocalDate.of(2023,4,10),true);
-//         EquipoData ed=new EquipoData();
-//         ed.modificarEquipo(eq);
-//       
+         Proyecto pro =new Proyecto(15,"Prueba","Prueba  datos",LocalDate.of(2022,3,1),true);
+         ProyectoData pd=new ProyectoData();
+         pd.crearProyectos(pro);
+          
+          
+         Equipo eq=new Equipo (5,pro,"Equipo 32",LocalDate.of(2023,4,10),true);
+         EquipoData ed=new EquipoData();
+         ed.modificarEquipo(eq);
        
-    
-}}
-
-        
+        }
     }
-    
-
