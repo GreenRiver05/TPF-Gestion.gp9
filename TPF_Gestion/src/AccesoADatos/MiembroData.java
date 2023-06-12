773aa811903c1bed4b200366c4da35153cdf49e0
alex-astudillo <astudillooalexx@gmail.com>
2023-06-12 02:11:31 -0300

    MiembroData y pruebas en el main

--- TPF_Gestion/src/AccesoADatos/MiembroData.java
+++ TPF_Gestion/src/AccesoADatos/MiembroData.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class MiembroData {
@@ -45,15 +46,41 @@ public class MiembroData {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Miembro" + ex.getMessage());
         }
     }
-
-    public Miembro buscarMiembro(int id) {
-        String sql = "SELECT * FROM `miembro` WHERE IdMiembro=?";
+    
+    public Miembro buscarMiembroDNI(int dni) {//FUNCA
+        String sql = "SELECT idMiembro,Dni, Apellido, Nombre, Estado FROM miembro WHERE Dni=?";
         Miembro miembro = null;
 
         PreparedStatement ps;
         try {
             ps = con.prepareStatement(sql);
-            ps.setInt(1, id);
+            ps.setInt(1, dni);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                miembro = new Miembro();
+                miembro.setIdMiembro(rs.getInt(1));
+                miembro.setDni(rs.getInt(2));
+                miembro.setApellido(rs.getString("Apellido"));
+                miembro.setNombre(rs.getString("Nombre"));
+                miembro.setEstado(rs.getBoolean("Estado"));
+            } else {
+                JOptionPane.showMessageDialog(null, "Miembro Inexistente");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Miembro " + ex.getMessage());
+        }
+
+        return miembro;
+    }
+    
+    public Miembro buscarMiembroPorApellido(String Apellido) {
+        String sql = "SELECT * FROM `miembro` WHERE Apellido=?";
+        Miembro miembro = null;
+
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, Apellido);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 miembro = new Miembro();
@@ -73,5 +100,88 @@ public class MiembroData {
 
         return miembro;
     }
+    public ArrayList<Miembro> listarMiembrosPorEstado(boolean estado) {//FUNCA
+        ArrayList<Miembro> miembross = new ArrayList();
+        String sql = "SELECT* FROM miembro WHERE Estado =? ORDER BY Apellido";
+
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setBoolean(1, estado);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                JOptionPane.showMessageDialog(null, "No se encontraron miembros con el estado " + estado);
+            } else {
+                do {
+                    Miembro miembro = new Miembro();
+                    miembro.setIdMiembro(rs.getInt("idMiembro"));
+                    miembro.setDni(rs.getInt("Dni"));
+                    miembro.setNombre(rs.getString("Nombre"));
+                    miembro.setApellido(rs.getString("Apellido"));
+                    miembro.setEstado(rs.getBoolean("Estado"));
+                    miembross.add(miembro);
+                } while (rs.next());
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Miembro " + ex.getMessage());
+        }
+        return miembross;
+
+    }
+      public void bajaMiembro(int id) {
+        String sql = "UPDATE miembro SET Estado=0 WHERE idMiembro=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Baja Realizada.");
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe Miembro con ese ID");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Miembro" + ex.getMessage());
+        }
+    }
+    
+    public void altaMiembro(int id) {//FUNCA
+
+        String sql = "UPDATE miembro SET Estado=1 WHERE idMiembro=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Alta Realizada.");
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe Miembro con ese ID");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Miembro" + ex.getMessage());
+        }
+
+    }
+    
+     public void modificarMiembro(Miembro miembro) {//FUNCA
+        String sql = "UPDATE miembro SET Dni=? ,Apellido=?, Nombre=?, Estado=? WHERE idMiembro=?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, miembro.getDni());
+            ps.setString(2, miembro.getApellido());
+            ps.setString(3, miembro.getNombre());
+            ps.setBoolean(4, miembro.isEstado());
+            ps.setInt(5, miembro.getIdMiembro());
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Miembro modificado exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Miembro no existente");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Miembro" + ex.getMessage());
+        }
+    }
+
 
 }
--- TPF_Gestion/src/tpf_gestion/TPF_Gestion.java
+++ TPF_Gestion/src/tpf_gestion/TPF_Gestion.java
@@ -7,6 +7,34 @@ import java.time.LocalDate;
 public class TPF_Gestion {
 
     public static void main(String[] args) {
+//------------------CREAR MIEMBRO---------------------------- 
+//        Miembro mie = new Miembro(2323323, "Urquiza", "Pepe",true );
+//        MiembroData md = new MiembroData();
+//        md.crearMiembro(mie);
+
+//        ................ BUSCAR MIEMBRO POR DNI ................  
+          MiembroData md = new MiembroData();
+          System.out.println("Miembro: " + md.buscarMiembroDNI(86782201).toString());
+
+//        ------------------BUSCAR MIEMBRO POR APELLIDO----------------------- 
+//        MiembroData md = new MiembroData();
+//        System.out.println("Apellido: " + md.buscarMiembroPorApellido("Clavito").toString());
+        
+//        ................ LISTAR MIEMBROS ................   
+//        MiembroData md = new MiembroData();
+//        System.out.println("Miembros: ");
+//        for (Miembro mie : md.listarMiembrosPorEstado(true)){
+//            System.out.println(mie.toString());
+//         }
+//        ................ BAJA Y ALTA MIEMBRO ................ 
+//        MiembroData md = new MiembroData();
+//        md.bajaMiembro(50);
+//        md.altaMiembro(50);
+
+//        ................ MODIFICAR MIEMBRO ................ 
+//        Miembro mie = new Miembro (50, 1234567, "Ramirez", "Pepe", true);
+//        MiembroData md = new MiembroData();
+//        md.modificarMiembro(mie);
 //     ..............CREAR EQUIPO.................
 //         Proyecto pro =new Proyecto("SSS","Prueba  datos",LocalDate.of(2022,3,1),true);
 //         ProyectoData pd=new ProyectoData();
@@ -35,14 +63,14 @@ public class TPF_Gestion {
 //        equi.altaEquipo(6);
 
 //*********MODIFICAR**************
-         Proyecto pro =new Proyecto(15,"Prueba","Prueba  datos",LocalDate.of(2022,3,1),true);
-         ProyectoData pd=new ProyectoData();
-         pd.crearProyectos(pro);
-          
-          
-         Equipo eq=new Equipo (5,pro,"Equipo 32",LocalDate.of(2023,4,10),true);
-         EquipoData ed=new EquipoData();
-         ed.modificarEquipo(eq);
-       
+//         Proyecto pro =new Proyecto(15,"Prueba","Prueba  datos",LocalDate.of(2022,3,1),true);
+//         ProyectoData pd=new ProyectoData();
+//         pd.crearProyectos(pro);
+//          
+//          
+//         Equipo eq=new Equipo (5,pro,"Equipo 32",LocalDate.of(2023,4,10),true);
+//         EquipoData ed=new EquipoData();
+//         ed.modificarEquipo(eq);
+//       
         }
     }
